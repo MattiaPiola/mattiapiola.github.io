0e3a414e48ad78a231e97d46f857dbafa3b29863
Mattia Piola <mattia.piola@gmail.com>
2023-09-04 17:10:52 +0200

    Added functions for managing table entries.

--- db_management.py
+++ db_management.py
@@ -1,10 +1,21 @@
-from tinydb import TinyDB, Query
+from tinydb import TinyDB, Query, operations
+
 
 db = TinyDB('./elo_database.json')
 players = db.table("players")
+matches = db.table('matches')
 
 def new_player(name, elo):
     players.insert({'name': name, 'elo': elo})
 
-# def update_elo(name, elo_delta):
+def new_match(name1, name2, elo1, elo2, exp1, exp2, res1, res2):
+    matches.insert({'player1':name1, 'player2': name2, 'elo1':elo1, 'elo2':elo2, 'exp1':exp1, 'exp2':exp2, 'res1':res1, 'res2':res2})
 
+def get_elo(name):
+    query = Query()
+    elo = players.search(query.name == name)[0]['elo']
+    return(elo)
+
+def update_elo(name,delta):
+    query = Query()
+    players.update(operations.add('elo',delta), query.name==name)
